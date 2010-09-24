e4972c89868a707187e5f94dfa29036921a7928f
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2010-09-24 08:22:07 +0200

    initial checkout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vienna2csv::Application.config.secret_token = 'cc322ed70f2f4e7e59594f8188a7ef21322bf3883865bd2e11f89b0b77ccc71f0bde1e3a9f1e4b1d29100541aa68e2cf7b0d2c1e04bb132c5589f1fa82c54fed'
