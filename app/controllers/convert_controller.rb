d154c39af767a573687eea2368fc2fa64747d51c
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2010-09-24 09:01:16 +0200

    most of the work is done

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,5 +1,27 @@
 class ConvertController < ApplicationController
   def index
   end
+  
+  def upload
+    uploaded_io = params[:vienna_file]
+    @input = uploaded_io.read
+
+    @output = @input
+    @output.gsub!(/ \\'e0 /, '";"')
+    @output.gsub!(/(";"\d{2}:\d{2})/, '\\1";"')
+    @output.gsub!(/\\\s*\\f1\\fs24/, '";"')
+    @output.gsub!(/\\\s*\\'a0\\\s*\\f0/, '";"')
+
+    @output.gsub!(/\\\s*$/, '"')
+    @output.gsub!(/^/, '"')
+
+    @output.gsub!(/;"[^"]+"$/, '')
+
+    @output = @output.split(/\\fs22 Grippe near:Paris within:25km - Twitter Search/)
+    
+    @output.delete_if { |l| ! l.match(/";"\d{2}:\d{2}";"/) }
+    
+        
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Vienna2csv::Application.routes.draw do
-  get "convert/index"
+get "convert/index"
+post "convert/upload"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +51,7 @@ Vienna2csv::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "convert#index"
 
   # See how all your routes lay out with "rake routes"
 
