d05eef46f945727d06cf3a592945582e5daf8f03
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2010-09-25 13:12:18 +0200

    fix chars

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,4 +1,5 @@
 class ConvertController < ApplicationController
+  
   def index
   end
   
@@ -17,11 +18,34 @@ class ConvertController < ApplicationController
 
     @output.gsub!(/;"[^"]+"$/, '')
 
+    @output.gsub!(/^([^"])/, '"\\1')
+
+    @output.gsub!(/\\'ee/, "î")
+    @output.gsub!(/\\'ef/, "ï")
+    @output.gsub!(/\\'b0/, "°")
+    @output.gsub!(/\\'9c/, "oe")
+    @output.gsub!(/\\'f4/, "ô")
+    @output.gsub!(/\\'fb/, "û")
+    @output.gsub!(/\\'92/, "'")
+    @output.gsub!(/\\'ea/, "ê")
+    @output.gsub!(/\\'e0/, "à")
+    @output.gsub!(/\\'e2/, "â")
+    @output.gsub!(/\\'e7/, "ç")
+    @output.gsub!(/\\'e8/, "è")
+    @output.gsub!(/\\'e9/, "é")
+
     @output = @output.split(/\\fs22 Grippe near:Paris within:25km - Twitter Search/)
     
     @output.delete_if { |l| ! l.match(/";"\d{2}:\d{2}";"/) }
     
-        
+    @filename = "output.csv"
+    
+#    f = File.new("public/#{@filename}", "w")
+#    f.write @output
+#    f.close
+    
+    
+
   end
 
 end
